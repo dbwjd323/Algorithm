b26192956a79a20be43e2f35c928aeeebb87640a
심유정 <131751945+dbwjd323@users.noreply.github.com>
2025-02-23 21:41:49 +0900

    [level 2] Title: 프로세스, Time: 0.66 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -0,0 +1,17 @@
+function solution(priorities, location) {
+   let queue = priorities.map((priority, index) => ({ priority, index }));
+    let count = 0;
+    
+    while(queue.length > 0){
+        let current = queue.shift(); 
+        
+        if(queue.some(task => task.priority > current.priority)){
+            queue.push(current);
+        } else {
+            count++;
+            if(current.index === location){
+                return count;
+            }
+        }
+    }
+}
